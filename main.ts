572966a5c8ba512d31e4619ec803ee3d6645beca
contextneeded <87148730+contextneeded@users.noreply.github.com>
2021-07-11 22:27:26 -0700

    added some endings and fixed some bugs

--- main.ts
+++ main.ts
@@ -1,64 +1,65 @@
-let _1st_place: Sprite = null
 let SecondPlace: Sprite = null
-let Value = randint(1, 50)
+let _1st_place: Sprite = null
 let input2 = 0
+let Value = randint(1, 50)
 let numberGuessesLeft = 7
-while (input2 != Value) {
+while (numberGuessesLeft > 0) {
     input2 = parseFloat(game.askForString("Guess the number between 1 and 50!"))
+    numberGuessesLeft += -1
     if (input2 < Value) {
         game.splash("Too low!")
     } else if (input2 > Value) {
         game.splash("Too high!")
-    }
-    numberGuessesLeft += -1
-    if (numberGuessesLeft <= 0) {
-        game.splash("No more guesses left")
-        game.over(false, effects.melt)
+    } else if (input2 == Value) {
+        if (numberGuessesLeft >= 3) {
+            _1st_place = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 5 5 5 . . . . . . . 
+                . . . . . 5 5 5 5 . . . . . . . 
+                . . . . 5 5 5 5 5 . . . . . . . 
+                . . . . 5 . 5 5 5 . . . . . . . 
+                . . . . . . 5 5 5 . . . . . . . 
+                . . . . . . 5 5 5 . . . . . . . 
+                . . . . . . 5 5 5 . . . . . . . 
+                . . . . . . 1 5 1 . . . . . . . 
+                . . . . 5 5 5 5 5 5 5 . . . . . 
+                . . . . 5 1 5 5 5 1 5 . . . . . 
+                . . . . 5 5 1 1 1 5 5 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Player)
+            _1st_place.say("Awesome!", 2500)
+            pause(5000)
+            game.over(true, effects.starField)
+        } else if (numberGuessesLeft > 2) {
+            SecondPlace = sprites.create(img`
+                . . . . . . 1 1 1 1 . . . . . . 
+                . . . . . 1 1 1 1 1 1 . . . . . 
+                . . . . 1 1 1 1 1 1 1 1 . . . . 
+                . . . 1 1 1 . . . 1 1 1 1 . . . 
+                . . 1 1 1 . . . . . 1 1 1 1 . . 
+                . . 1 1 . . . . . . . 1 1 1 . . 
+                . . . . . . . . . . . 1 1 1 . . 
+                . . . . . . . . . . . 1 1 1 . . 
+                . . . . . . . . . . . 1 1 1 . . 
+                . . . . . . . . . . 1 1 1 . . . 
+                . . . . . . . . . 1 1 1 . . . . 
+                . . . . . . . 1 1 1 1 . . . . . 
+                . . . . . . 1 1 1 1 . . . . . . 
+                . . . . . 1 1 1 1 . . . . . . . 
+                . . . . 1 1 1 1 1 1 1 1 1 1 . . 
+                . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+                `, SpriteKind.Player)
+            SecondPlace.say("Good, you guessed right!", 4500)
+            pause(5000)
+            game.over(true, effects.confetti)
+        } else {
+            game.splash("At least you guessed it...")
+            game.over(true, effects.clouds)
+        }
     }
     game.splash("You have " + numberGuessesLeft + " guesses left")
 }
-if (numberGuessesLeft < 3) {
-    SecondPlace = sprites.create(img`
-        . . . . . . 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 1 1 . . . . . 
-        . . . . 1 1 1 1 1 1 1 1 . . . . 
-        . . . 1 1 1 . . . 1 1 1 1 . . . 
-        . . 1 1 1 . . . . . 1 1 1 1 . . 
-        . . 1 1 . . . . . . . 1 1 1 . . 
-        . . . . . . . . . . . 1 1 1 . . 
-        . . . . . . . . . . . 1 1 1 . . 
-        . . . . . . . . . . . 1 1 1 . . 
-        . . . . . . . . . . 1 1 1 . . . 
-        . . . . . . . . . 1 1 1 . . . . 
-        . . . . . . . 1 1 1 1 . . . . . 
-        . . . . . . 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 . . . . . . . 
-        . . . . 1 1 1 1 1 1 1 1 1 1 . . 
-        . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
-        `, SpriteKind.Player)
-    SecondPlace.say("Good, you guessed right!", 1200)
-    pause(1200)
-    game.over(true, effects.clouds)
-} else if (numberGuessesLeft < 5) {
-    _1st_place = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . 5 5 5 . . . . . . . 
-        . . . . . 5 5 5 5 . . . . . . . 
-        . . . . 5 5 5 5 5 . . . . . . . 
-        . . . . 5 . 5 5 5 . . . . . . . 
-        . . . . . . 5 5 5 . . . . . . . 
-        . . . . . . 5 5 5 . . . . . . . 
-        . . . . . . 5 5 5 . . . . . . . 
-        . . . . . . 1 5 1 . . . . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . 5 1 5 5 5 1 5 . . . . . 
-        . . . . 5 5 1 1 1 5 5 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
-    _1st_place.say("Great job, you guessed right!", 1200)
-    pause(1200)
-    game.over(true, effects.starField)
-}
+game.over(false, effects.melt)
